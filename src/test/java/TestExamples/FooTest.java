b13052433a51f098933edbe979c6582284f4ce06
Bharat.Hiremath <bharathphm@yahoo.in>
2020-04-11 18:39:01 +0530

    changes

--- src/test/java/TestExamples/FooTest.java
+++ src/test/java/TestExamples/FooTest.java
@@ -6,11 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FooTest {
 
+
     @Test
     public void getBarTest() {
         Foo foo = new Foo();
         String result = foo.bar();
         assertEquals("FooBar", result);
+
+
+    }
+
+    @Test
+    public void getBarTest_returnsFailure() {
+        Foo foo = new Foo();
+        String result = foo.bar();
+        assertNotEquals("FoovcBar", result);
 
 
     }
